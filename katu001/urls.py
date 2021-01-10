3be0f33b019384caeae734b8119710f6965eb004
Katuhito Hara <katuhitohara@gmail.com>
2021-01-10 19:39:44 +0900

    allauth, diary,model, migration

--- katu001/urls.py
+++ katu001/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import static
+from . import settings_common, settings_dev
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('katuapp.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
+
+# 開発サーバーでメディア配信できるようにする設定
+urlpatterns += static(settings_common.MEDIA_URL, document_root=settings_dev.MEDIA_ROOT)
